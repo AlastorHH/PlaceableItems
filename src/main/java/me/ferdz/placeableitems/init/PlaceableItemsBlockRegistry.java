d325c39acf22bb76c7ae7535011da041417d013e
Ferdzz <frederic.deschenes1@gmail.com>
2019-08-26 20:16:56 -0400

    Working implementation of component fillStateContainer

--- src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
+++ src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
@@ -1,11 +1,10 @@
 package me.ferdz.placeableitems.init;
 
 import me.ferdz.placeableitems.block.PlaceableItemsBlock;
+import me.ferdz.placeableitems.block.PlaceableItemsBlockBuilder;
 import me.ferdz.placeableitems.block.component.impl.BiPositionBlockComponent;
 import me.ferdz.placeableitems.block.component.impl.BoneBlockComponent;
 import me.ferdz.placeableitems.utils.VoxelShapesUtil;
-import me.ferdz.placeableitems.wiki.Wiki;
-import me.ferdz.placeableitems.wiki.WikiDataGenerator;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -13,32 +12,24 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-// TODO: Make this class streamlined for registration
-
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PlaceableItemsBlockRegistry {
 
-    private static final boolean GENERATE_WIKI = System.getenv().containsKey("GENERATE_WIKI");
-
-    @Wiki(description = "A bone")
     public static PlaceableItemsBlock BONE;
     public static PlaceableItemsBlock APPLE;
 
     @SubscribeEvent
     public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
-        BONE = new PlaceableItemsBlock()
-                .setShape(VoxelShapesUtil.create(0, 0, 0, 16, 4, 16))
+        BONE = new PlaceableItemsBlockBuilder()
                 .addComponent(new BoneBlockComponent())
+                .build()
+                .setShape(VoxelShapesUtil.create(16, 4, 16))
                 .register("bone_block", Items.BONE);
-        APPLE = new PlaceableItemsBlock()
-                .setShape(VoxelShapesUtil.create(5, 9, 5))
+        APPLE = new PlaceableItemsBlockBuilder()
                 .addComponent(new BiPositionBlockComponent())
+                .build()
+                .setShape(VoxelShapesUtil.create(5, 9, 5))
                 .register("apple_block", Items.APPLE);
-
-        if(GENERATE_WIKI) {
-            WikiDataGenerator generator = new WikiDataGenerator();
-            generator.generate("test.json");
-        }
     }
 
     @SubscribeEvent
